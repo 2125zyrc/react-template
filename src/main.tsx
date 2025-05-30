3dacef42c847996c225dfd9d4f33854b4e5f21b5
Louis <2125zyrc.@gmail.com>
2025-05-30 15:49:54 +0800

    修复已知bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-// 创建 QueryClient 实例
-const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 )
